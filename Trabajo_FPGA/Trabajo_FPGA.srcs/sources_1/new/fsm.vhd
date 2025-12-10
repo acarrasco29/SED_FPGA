cfc359f56b61704c6b48e9ef2345189d9b6c6bdf
jcarnero2004 <coque.carnero@gmail.com>
2025-12-10 13:11:18 +0100

    Fichero de restricciones y Arquitectura FSM
    He añadido el fichero de restricciones. Además, se ha modificado la arquitectura de FSM metiendo la lógica de los estados con sus salidas.

--- Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/fsm.vhd
+++ Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/fsm.vhd
@@ -14,7 +14,9 @@ entity fsm is
         down_bttn   : in STD_LOGIC;
         slct_bttn   : in std_logic;
         
-        duty_vector : out std_logic_vector (2 downto 0)
+        duty_R : out std_logic_vector (2 downto 0);
+        duty_G : out std_logic_vector (2 downto 0);
+        duty_B : out std_logic_vector (2 downto 0)
         
     );
 end fsm;
@@ -25,6 +27,7 @@ architecture Behavioral of fsm is
     signal current_state : states := S0;
     signal next_state    : states;
     signal d_R, d_G, d_B : std_logic_vector (2 downto 0);
+    signal up_bttn_R, up_bttn_G, up_bttn_B, down_bttn_R, down_bttn_G, down_bttn_B : STD_LOGIC := '0';
 
     component duty
         generic (
@@ -48,8 +51,8 @@ begin
             bit_colours => bit_colours
         )    
         port map (
-            up_bttn => up_bttn,
-            down_bttn => down_bttn,
+            up_bttn => up_bttn_R,
+            down_bttn => down_bttn_R,
             CLK => CLK,
             RST => RST,
             duty => d_R        
@@ -60,8 +63,8 @@ begin
             bit_colours => bit_colours
         )    
         port map (
-            up_bttn => up_bttn,
-            down_bttn => down_bttn,
+            up_bttn => up_bttn_G,
+            down_bttn => down_bttn_G,
             CLK => CLK,
             RST => RST,
             duty => d_G   
@@ -72,11 +75,83 @@ begin
             bit_colours => bit_colours
         )    
         port map (
-            up_bttn => up_bttn,
-            down_bttn => down_bttn,
+            up_bttn => up_bttn_B,
+            down_bttn => down_bttn_B,
             CLK => CLK,
             RST => RST,
             duty => d_B        
         );
-
-end Behavioral;
+    
+    state_register: process (RST, CLK)
+    begin
+        if RST = '1' then
+           current_state <= s0;
+        elsif rising_edge(CLK)then
+            current_state <= next_state;
+        end if;
+    end process;
+    
+     nextstate_decod: process (SLCT_BTTN, current_state)
+     begin
+        next_state <= current_state;
+        case current_state is
+            when S0 =>
+                if SLCT_BTTN = '1' then
+                    next_state <= SR;
+                end if;
+            when SR =>
+                if SLCT_BTTN = '1' then
+                    next_state <= SG;
+                end if;
+            when SG =>
+                if SLCT_BTTN = '1' then
+                    next_state <= SB;
+                end if;
+            when SB =>
+                if SLCT_BTTN = '1' then
+                    next_state <= SR;
+                end if;
+            when others =>
+                next_state <= S0;
+        end case;
+     end process;
+     
+     output_decod: process (current_state)
+     begin
+        case current_state is
+            when SR =>
+                up_bttn_R <= up_bttn;
+                down_bttn_R <= up_bttn;
+                up_bttn_G <= '0';
+                down_bttn_G <= '0';
+                up_bttn_B <= '0';
+                down_bttn_B <= '0';
+            when SG =>
+                up_bttn_R <= '0';
+                down_bttn_R <= '0';
+                up_bttn_G <= up_bttn;
+                down_bttn_G <= up_bttn;
+                up_bttn_B <= '0';
+                down_bttn_B <= '0';
+            when SB =>
+                up_bttn_R <= '0';
+                down_bttn_R <= '0';
+                up_bttn_G <= '0';
+                down_bttn_G <= '0';
+                up_bttn_B <= up_bttn;
+                down_bttn_R <= up_bttn;
+            when others =>
+                up_bttn_R <= '0';
+                down_bttn_R <= '0';
+                up_bttn_G <= '0';
+                down_bttn_G <= '0';
+                up_bttn_B <= '0';
+                down_bttn_B <= '0';
+        end case;
+     end process;
+     
+     duty_R <= D_R;
+     duty_G <= D_G;
+     duty_B <= D_B;
+     
+ end Behavioral;

eebf01d8bb0ae154337f7577e23d5c833395df45
Acastrog-13 <161310060+Acastrog-13@users.noreply.github.com>
2025-12-12 11:16:14 +0100

    Arquitectura TOP
    Se completa la arquitectura TOP con el mapeo de señales.

--- Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/TOP.vhd
+++ Trabajo_FPGA/Trabajo_FPGA.srcs/sources_1/new/TOP.vhd
@@ -16,8 +16,10 @@ entity TOP is
 end TOP;
 
 architecture Behavioral of TOP is
-    constant bit_colours            : natural := 4;
-    signal duty_R, duty_G, duty_B   : std_logic_vector(bit_colours-1 downto 0);
+    constant bit_colours                 : natural := 4;
+    signal duty_R, duty_G, duty_B        : std_logic_vector(bit_colours-1 downto 0);
+    signal up_sync, down_sync, slct_sync : std_logic;
+    signal up_stp, down_stp, slct_edge   : std_logic;
     
      component SYNCHRNZR
         port (
@@ -29,60 +31,105 @@ architecture Behavioral of TOP is
    
     component EDGEDTCTR
         port (
-            CLK : in std_logic;
+            CLK     : in std_logic;
             SYNC_IN : in std_logic;
-            EDGE : out std_logic
+            EDGE    : out std_logic
         );
      end component;
      
      component STEPDTCTR
         port (
-            CLK : in std_logic;
+            CLK     : in std_logic;
             SYNC_IN : in std_logic;
-            STP : out std_logic
+            STP     : out std_logic
         );
      end component;
      
      component FSM
         port (
-            up_bttn   : in STD_LOGIC;
-            down_bttn : in STD_LOGIC;
-            CLK       : in STD_LOGIC;
-            RST       : in STD_LOGIC;
-            duty      : out STD_LOGIC_VECTOR ( bit_colours-1 downto 0)
+            CLK         : in STD_LOGIC;
+            RST         : in STD_LOGIC;
+            
+            up_bttn     : in STD_LOGIC;
+            down_bttn   : in STD_LOGIC;
+            slct_bttn   : in std_logic;
+            
+            duty_R      : out std_logic_vector (2 downto 0);
+            duty_G      : out std_logic_vector (2 downto 0);
+            duty_B      : out std_logic_vector (2 downto 0)
+        
         );
      end component;
      
      component PWM
         port (
-            CLK         : in  std_logic; 
-            RST         : in  std_logic; 
-            duty_R      : in  std_logic_vector(bit_colours-1 downto 0); 
-            duty_G      : in  std_logic_vector(bit_colours-1 downto 0); 
-            duty_B      : in  std_logic_vector(bit_colours-1 downto 0);    
-            RGB         : out std_logic_vector(2 downto 0); 
-            RGB_n       : out std_logic_vector(2 downto 0)        
+            CLK     : in  std_logic; 
+            RST     : in  std_logic; 
+            duty_R  : in  std_logic_vector(bit_colours-1 downto 0); 
+            duty_G  : in  std_logic_vector(bit_colours-1 downto 0); 
+            duty_B  : in  std_logic_vector(bit_colours-1 downto 0);    
+            RGB     : out std_logic_vector(2 downto 0); 
+            RGB_n   : out std_logic_vector(2 downto 0)        
         );
      end component;
     
 begin
-    --inst_SYNCHRNZR : SYNCHRNZR PORT MAP (
-    --        CLK => CLK;
-    --        ASYNC_IN 
-    --        SYNC_OUT 
-    --);
-    --inst_EDGEDTCTR : EDGEDTCTR PORT MAP (
+    up_SYNCHRNZR : SYNCHRNZR PORT MAP (
+            CLK       => CLK,
+            ASYNC_IN  => up_bttn,
+            SYNC_OUT  => up_sync
+    );
     
-    --);
-    --inst_STEPDTCTR : STEPDTCTR PORT MAP (
+    down_SYNCHRNZR : SYNCHRNZR PORT MAP (
+            CLK       => CLK,
+            ASYNC_IN  => down_bttn,
+            SYNC_OUT  => down_sync
+    );
     
-    --);
-    --inst_FSM : FSM PORT MAP (
+    slct_SYNCHRNZR : SYNCHRNZR PORT MAP (
+            CLK       => CLK,
+            ASYNC_IN  => slct_bttn,
+            SYNC_OUT  => slct_sync
+    );
     
-    --);
-    --inst_PWM : PWM PORT MAP (
+    slc_EDGEDTCTR : EDGEDTCTR PORT MAP (
+            CLK      => CLK,
+            SYNC_IN  => slct_sync,
+            EDGE     => slct_edge
+    );
+    up_STEPDTCTR : STEPDTCTR PORT MAP (
+            CLK      => CLK,
+            SYNC_IN  => up_sync,
+            STP      => up_stp
+    );
     
-    --);
+    down_STEPDTCTR : STEPDTCTR PORT MAP (
+            CLK      => CLK,
+            SYNC_IN  => down_sync,
+            STP      => down_stp
+    );
+    
+    inst_FSM : FSM PORT MAP (
+            CLK        => CLK,
+            RST        => RST,
+            
+            up_bttn    => up_stp,
+            down_bttn  => down_stp,
+            slct_bttn  => slct_edge,
+           
+            duty_R     => duty_R,
+            duty_G     => duty_G,
+            duty_B     => duty_B
+    );
+    inst_PWM : PWM PORT MAP (
+            CLK     => CLK,
+            RST     => RST,
+            duty_R  => duty_R,
+            duty_G  => duty_G,
+            duty_B  => duty_B,
+            RGB     => RGB,
+            RGB_n   => RGB_n
+    );
     
 
 
